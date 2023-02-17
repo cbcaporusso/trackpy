38ff6e68ce6a6b7144d091473ccea73c0dffafdd
Claudio B. Caporusso <cbcaporusso@gmail.com>
2023-02-18 00:50:35 +0100

    moved hex functions in another file

--- src/utils.py
+++ src/utils.py
@@ -1,32 +1,4 @@
-import numpy as np
-import subprocess
-
 from typing import Tuple
-
-def compute_local_hexatic(file_path, time):
-    """
-    Compute the local hexatic order parameter for a given time, calling an external bash script.
-
-    Parameters
-    ----------
-    N : int
-        Number of particles in the system.
-    temp : float
-        Temperature of the system.
-    omega : float
-        Frequency of the chiral force.
-    rho : float 
-        Density of the system.
-    time : int  
-        Time at which to compute the local hexatic order parameter.
-
-    Returns
-    -------
-    None
-
-    """
-    process_status = subprocess.run(["/gpfs/projects/ub35/demian/chiral/elaborate.sh", file_path, str(time)])
-    return process_status
 
 def extract_params_from_path(filepath: str) -> Tuple[int, float, float, float]:
     """
@@ -77,13 +49,13 @@ def _find_param_index_in_path(filepath: str, param: str) -> int:
 
     """
 
-    list = filepath.split('/')
+    split_path_list = filepath.split('/')
     # check partial matching pattern 'rho_*' in list
     # and extract the index of the first match
     try:
-        index = next(i for i, s in enumerate(list) if param in s)
+        index = next(i for i, s in enumerate(split_path_list) if param in s)
     except StopIteration:
-        print("No rho parameter found in path.")
+        print("No parameter found in path.")
         exit()
 
     return index
